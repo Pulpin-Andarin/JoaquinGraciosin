1711e4ec3018c9398f2326a5a816e33da2e0d16f
AlbertoBtt <54907618+AlbertoBtt@users.noreply.github.com>
2024-01-26 20:37:15 +0100

    Project Init

---
+++ Source/JoaquinGraciosin/JoaquinGraciosinGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JoaquinGraciosinGameMode.h"
+#include "JoaquinGraciosinCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AJoaquinGraciosinGameMode::AJoaquinGraciosinGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/JoaquinGraciosin/JoaquinGraciosinGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JoaquinGraciosinGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AJoaquinGraciosinGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AJoaquinGraciosinGameMode();
+};
+
+
+
