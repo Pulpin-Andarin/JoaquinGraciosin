103588e75ebf5a6a194dab60bd5aa82185d69485
AlbertoBtt <54907618+AlbertoBtt@users.noreply.github.com>
2024-01-27 01:52:58 +0100

    Que entre la china

--- Source/JoaquinGraciosin/JoaquinGraciosinGameMode.cpp
+++ Source/JoaquinGraciosin/JoaquinGraciosinGameMode.cpp
@@ -7,7 +7,7 @@
 AJoaquinGraciosinGameMode::AJoaquinGraciosinGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/JoaquinGraciosin/Blueprints/Player/MyPlayerCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
